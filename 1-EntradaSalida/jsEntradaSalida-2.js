cbfc0242fb3a2a7d27ca694957f657e841b56e88
fedefhm <36174149+fedefhm@users.noreply.github.com>
2018-02-05 20:54:39 -0300

    ejercicio 1 y 2
    ver el alert, prompt, variables y concatenar

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,26 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	//esto es un comentario de una linea
+//alert("anda");
+// toda linea de intruccion, termina con punto y coma (;)
+
+var nombre;
+/*
+
+con el "barra asterisco", defino bloque de comentarios (abro con barra asterisco y lo cierro a la inversa)
+var nombre;
+alert("nombre");
+alert(nombre);//si lo dejo asi, me va a mostras "undefinied" porque no defini valor de variable.
+nombre="Federico";
+alert(nombre)
+*/
+
+
+
+nombre=prompt("ingrese su nombre","natalia natalia");
+alert(nombre);
+
+alert("su nombre es: "+ nombre);
 }
 
