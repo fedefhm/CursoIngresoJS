37983bdd917996cce486c3b6bea7b581dae56afc
fedefhm <36174149+fedefhm@users.noreply.github.com>
2018-02-06 22:04:28 -0300

    Instruccion "if"
    Ejercicios 1 a 6. Terminar el resto.

---
+++ jsInstruccionIF (1).js
@@ -0,0 +1,12 @@
+function Mostrar()
+{
+//tomo la edad  
+
+var edad;
+edad=15;
+if(edad==15)             // un igual asigna valor; dos iguales compara contra valor de verdad	
+{
+alert("niña bonita");
+}
+
+}//FIN DE LA FUNCIÓN
---
+++ jsInstruccionIF (4).js
@@ -0,0 +1,30 @@
+function Mostrar()
+{
+//tomo la edad  
+
+var edad
+edad=document.getElementById('edad').value;
+
+if(edad>12 && edad<17)
+
+{
+
+	alert("es un adolescente")
+}
+
+//con el doble "&&" le agrego un "y" logico
+// el "o" logico es || y se hace alt Gr + 1
+
+/*if(edad>12)
+
+{
+	if(edad<17)
+	{
+
+		alert("")
+	}
+*/
+
+
+
+}//FIN DE LA FUNCIÓN
