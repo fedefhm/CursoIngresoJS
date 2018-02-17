acc20054ffb94da2c11525eca02d2b078e8a5f7c
fedefhm <36174149+fedefhm@users.noreply.github.com>
2018-02-17 11:59:44 -0300

    Instruccion If (Trabajo en clase)
    Quedan cosas para terminar.

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -3,4 +3,23 @@ function Mostrar()
 	//Genero el número RANDOM entre 1 y 10 
 	
 
+var numeroRandom;
+
+numeroRandom=Math.floor((Math.random() * 10) + 1);
+	if(numeroRandom>8)
+	{
+		alert("Excelente "+ numeroRandom);
+
+			}
+			if(numeroRandom<8 && numeroRandom>3)
+	{
+		alert("Aprobó "+ numeroRandom);
+
+			}
+			if(numeroRandom<4)
+	{
+		alert("Next time "+ numeroRandom);
+
+			}
+
 }//FIN DE LA FUNCIÓN
