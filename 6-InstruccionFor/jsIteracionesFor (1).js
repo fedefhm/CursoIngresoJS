4f8c610871a0c49d1beb4a56dd45c772eb805b42
fedefhm <36174149+fedefhm@users.noreply.github.com>
2018-02-20 21:59:17 -0300

    Ejercicios 1 y 2 hechos en clase
    Falta terminar.

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,85 @@
 function Mostrar()
 {
 
+// var contador=0; puedo definir la variable y sacarla del primer lugar del parentesis del for
+
+/* for(contador=0;contador<10;) //los dos punto y coma tienen que quedar si le saco el "contador++" de atrás y ponerlo en la llave
+{
+	contador++
+	document.write(contador);
+	*/
+
+
+ 
+//con esto tambien funciona. El parentesis del for es una iteración infinita escrita así; conlo cual debo acotarla abajo con el if
+var contador=0;
+for(;;)
+{
+	contador++;
+	document.write("<br>"+contador);
+	if(contador==10)
+	{
+		document.write("break"+contador);
+		break;
+	}
+	if(contador==5)
+	{
+	continue;
+	}
+	document.write("<br>continue"+contador);
+	
 }
+
+}
+
+/*
+
+var contadorDePositivos=0;
+var contadorDeNegativos=0;
+var contadorDeCeros=0;
+var contadorDePar=0;
+var contadorDeImpar=0;
+var contador50=0;
+
+for(contador=-100;contador<100;contador++)
+{
+	if(contador>0)
+	{
+		contadorDePositivos++;
+	}
+	else{
+		if(contador<0)
+		{
+			contadorDeNegativos++;
+		}else
+		{
+			contadorDeCeros++
+		}
+
+
+	}
+	if(contador%2==0 && contador!=0)
+	{
+		contadorDePar++;
+	}else{
+		if(contador!=0)
+		{
+			contadorDeImpar++;
+		}
+	}
+
+	switch(contador)
+	{
+		case 50:
+		contador50 ++;
+		break;
+	}
+}
+
+document.write("Positivos"+contadorDePositivos);
+document.write("Negativos"+contadorDeNegativos);
+document.write("Ceros"+contadorDeCeros);
+document.write("par"+contadorDePar);
+document.write("impar"+contadorDeImpar);
+document.write("contador50"+contador50);
+*/
--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,54 @@
 function Mostrar()
 {
 
+var numeroRandom;
+var contador1=0;
+var contador2=0;
+var contador3=0;
+var contador4=0;
+var contador5=0;
+var contador6=0;
+var contador7=0;
+var contador8=0;
+var contador9=0;
+
+for(contador=0;contador<100;contador++)
+
+{
+	numeroRandom=Math.floor(Math.random() * 10);
+//document.write(numeroRandom);
+
+switch(numeroRandom)
+{
+	case 0:
+		contador0++;
+		break;
+	case 1:
+		contador1++;
+	case 2:
+		contador2++;
+		break;
+	case 3:
+		contador3++;
+		break;
+	case 4:
+		contador4++;
+		break;
+	case 5:
+		contador5++;
+		break;
+	case 7:
+		contador7++;
+		break;
+	case 8:
+		contador8++;
+		break;
+	case 9:
+		contador9++;
+		break;
+
+}
+
+}
 
 }
