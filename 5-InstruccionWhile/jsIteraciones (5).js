34824c55eb99b8671b1e42b70f44a04bebb13544
fedefhm <36174149+fedefhm@users.noreply.github.com>
2018-02-26 19:09:56 -0300

    Ejercicios 5 y 6
    continuar el resto

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -2,16 +2,15 @@ function Mostrar()
 {
 
 var sexo = prompt("ingrese f ó m");
-sexo=f;
-sexo=m;
-document.getElementById('Sexo').value=sexo;
 
-	while(sexo!=f || sexo!=m)
+
+
+	while(sexo!="f" && sexo!="m")
 		{
 			alert("Ingrese sexo correcto");
 			sexo = prompt("ingrese f ó m");
 		}
 
-	
+document.getElementById('Sexo').value=sexo;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -4,21 +4,20 @@ function Mostrar()
 	var numero;
 	var contador=0;
 	var acumulador=0;
-	var suma;
-	var promedio;
-
-	document.getElementById('suma').value=acumulador;
-	document.getElementById('promedio').value=acumulador/5;
-	numero= parseInt(numero);
-
-	while(acumulador<5)
+	
+	while(contador<5)
 	{
-		acumulador++;
-		numero= prompt(numero);
+		
+		numero= prompt("Ingrese numero");
+		contador++;
+		numero= parseInt(numero);
+		acumulador= acumulador+numero; // en la primera vuelta, acumuladores cero más el número ingresado
+		// en la segunda, será el acumulado, más el nuevo número y así.A la quinta vez, sale.
 		
 
 	}
 
-	
+	document.getElementById('suma').value= acumulador;
+	document.getElementById('promedio').value= acumulador/5;
 
 }//FIN DE LA FUNCIÓN
