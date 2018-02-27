e1914fbb37179209f3f78fe2ae61b68210fd51ac
fedefhm <36174149+fedefhm@users.noreply.github.com>
2018-02-26 21:56:07 -0300

    Ejercicios 7 y 10 terminados (corregidos)
    Ahora si: guía completa

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,29 +1,31 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-
-
 var numeroRandom;
 numeroRandom= Math.floor((Math.random() * 10) + 1);
 
 if(numeroRandom>8)
-		
 	{
-	alert(“Excelente”+ numeroRandom);
-
+	alert("Excelente: "+numeroRandom);
 	} 
-		else
-			{
-				if(numeroRandom<4)
-					{
-						alert(“Vamos que la próxima se puede: ” + numeroRandom);
-					}
+	
+	else
+	{
+		if(numeroRandom<4)
+		{
+			alert("Vamos que la próxima se puede: " + numeroRandom);
+		}
 
-			} else
-				{
-					alert(“Aprobó:” + numeroRandom); 
-				}
+		else
+		{
+			alert("Aprobó: " + numeroRandom); 
+		}
+	}
 }
+
+
+
+
 
 
 /*var numeroRandom;
