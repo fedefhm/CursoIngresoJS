ceae15511be65d4b546bbb2d65628906153a7182
fedefhm <36174149+fedefhm@users.noreply.github.com>
2018-03-01 00:56:35 -0300

    Ejercicios 7 y 8 resueltos.
    Trabajo en casa. Faltan 9 y 10.

--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -1,11 +1,39 @@
 function Mostrar()
-{
-
+{	/*
 	var contador=0;
 	var acumulador=0;
-	var respuesta='si';
+	var respuesta="y";
 
+	while(respuesta=="y")
+		{	
+			numero=prompt("Ingrese número");
+			parseInt(numero); //despues de tomar el numero, antes de parsearlo,
+			// debo poner el contador; sino no funciona.
+			acumulador=acumulador+numero;
+			contador++;
+			respuesta= prompt("¿continuar?");
+		}
+	document.getElementById('suma').value= acumulador;
+	document.getElementById('promedio').value=acumulador/contador;
 
+*/
+	var contador=0;
+	var acumulador=0;
+	var respuesta='y';
+
+	while(respuesta == "y")
+	{
+		
+		numero = prompt ("ingrese numero");
+
+		contador++; 
+
+		numero = parseInt(numero);
+
+		acumulador= acumulador+numero;
+
+		respuesta= prompt("Quiere ingresar otro numero?");
+	}
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
 
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -5,9 +5,29 @@ function Mostrar()
 	var positivo=0;
 	var negativo=1;
 	
-	var respuesta='si';
+	var respuesta=true;
 
+	while(respuesta == true)
+	{
+		
+		numero = prompt ("ingrese numero");
 
+		contador++;
+
+		numero = parseInt(numero);
+
+		if(numero<0)
+		{
+			negativo= negativo * numero;
+		}
+		if(numero>0)
+		{
+			positivo= positivo+numero;
+		}
+
+		respuesta= confirm("Quiere ingresar otro numero?");
+		
+	}
 document.getElementById('suma').value=positivo;
 document.getElementById('producto').value=negativo;
 
